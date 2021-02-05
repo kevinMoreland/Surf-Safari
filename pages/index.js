3914b980f04b25dc7439db6ce935df79fab24660
kevinMoreland <kmmorela@calpoly.edu>
2021-02-05 08:13:46 -0800

    Made map fill entire screen

--- pages/index.js
+++ pages/index.js
@@ -14,27 +14,22 @@ export default function Home() {
 
     let map = new mapboxgl.Map({
       container: "my-map",
-      style: "mapbox://styles/mapbox/streets-v11",
+      style: "mapbox://styles/mapbox/satellite-v9",
       center: [-77.02, 38.887],
-      zoom: 12.5,
-      pitch: 45,
-      maxBounds: [
-        [-77.875588, 38.50705], // Southwest coordinates
-        [-76.15381, 39.548764], // Northeast coordinates
-      ],
+      zoom: 1,
     });
   }, []);
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link href='https://api.mapbox.com/mapbox-gl-js/v2.0.1/mapbox-gl.css' rel='stylesheet' />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <div id="my-map" style={{height: 600, width: 600}}/>
+      <main className={styles.flexContainer}>
+        <div id="my-map" className={styles.fillingDiv}/>
       </main>
     </div>
   )
