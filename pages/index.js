add41273e56ddb5d04eca458b71151fe93ceb7f4
kevinMoreland <kmmorela@calpoly.edu>
2021-02-04 16:44:43 -0800

    Added mapbox to project

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,41 @@
 import Head from 'next/head'
+import { useEffect, useState } from "react";
 import styles from '../styles/Home.module.css'
+const mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
 
 export default function Home() {
+  const [pageIsMounted, setPageIsMounted] = useState(false);
+  const [Map, setMap] = useState();
+
+  mapboxgl.accessToken = 'pk.eyJ1Ijoia2V2aW5tb3JlbGFuZCIsImEiOiJja2hyMWRwczMwcWRqMnNvMnRldzFjYmtzIn0.5zO1V-Zr91Rsq_1dSHFYVg'
+
+  useEffect(() => {
+    setPageIsMounted(true);
+
+    let map = new mapboxgl.Map({
+      container: "my-map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-77.02, 38.887],
+      zoom: 12.5,
+      pitch: 45,
+      maxBounds: [
+        [-77.875588, 38.50705], // Southwest coordinates
+        [-76.15381, 39.548764], // Northeast coordinates
+      ],
+    });
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
+        <link href='https://api.mapbox.com/mapbox-gl-js/v2.0.1/mapbox-gl.css' rel='stylesheet' />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <div id="my-map" style={{height: 600, width: 600}}/>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
