8c34f12ad4ac5c8bf1a82287511ecfea74aa858c
kevinMoreland <kmmorela@calpoly.edu>
2021-03-24 10:53:27 -0700

    Updated tracking id

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ import { mapModes, availableMaps } from "../map/mapModes";
 import { fetcher } from "../utilities/fetcher";
 import 'fontsource-roboto';
 import ReactGA from 'react-ga';
-ReactGA.initialize('UA-192976146-1');
+ReactGA.initialize('UA-192919844-1');
 //home for the demo version
 export default function Home() {
   const [pageIsMounted, setPageIsMounted] = useState(false);
