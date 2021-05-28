3cf769381825dab49c65f47644c5a7de8f280782
kevinMoreland <kmmorela@calpoly.edu>
2021-05-27 17:45:46 -0700

    modified png ending because of case sensitivity

--- components/FullScreenDialog/HelpContent/HelpContent.js
+++ components/FullScreenDialog/HelpContent/HelpContent.js
@@ -18,27 +18,27 @@ export default function HelpContent(props) {
             <br />
             <Typography variant="h4">How do you use Surf Safari?</Typography>
             <Typography variant="body1">Most of the features of Surf safari can be accessed by clicking the map at a location you want to analyze.</Typography>
-            <img style={{width: imageWidth, paddingTop: '1vh'}} src={"/rightClickMenu.png"}/>
+            <img style={{width: imageWidth, paddingTop: '1vh'}} src={"/rightClickMenu.PNG"}/>
 
             <br />
             <br />
             <Typography variant="h4">Saving Surf Spots</Typography>
             <Typography variant="body1">In order to save a surf spot, you must create an account and log in first. Accounts can be accessed from the top left corner of the website at the profile button.</Typography>
-            <img style={{width: imageWidth, paddingTop: '1vh'}} src={"/profileLocation.png"}/>
+            <img style={{width: imageWidth, paddingTop: '1vh'}} src={"/profileLocation.PNG"}/>
             <Typography variant="body1">Once you are logged into your account, you can click the map and see the option to save a surf spot.</Typography>
-            <img style={{width: imageWidth, paddingTop: '1vh'}} src={"/SaveSurfSpotOption.png"}/>
+            <img style={{width: imageWidth, paddingTop: '1vh'}} src={"/SaveSurfSpotOption.PNG"}/>
             <Typography variant="body1">When this option is clicked, a side bar will display where you can add a title and description to your discovered spot, and it will be saved for future reference!</Typography>
-            <img style={{width: imageWidth, paddingTop: '1vh'}} src={"/saveSpotSidebar.png"}/>
+            <img style={{width: imageWidth, paddingTop: '1vh'}} src={"/saveSpotSidebar.PNG"}/>
 
             <br />
             <br />
             <Typography variant="h4">Switching Map Views</Typography>
             <Typography variant="body1">At the bottom left corner of the website, you can toggle between a satellite view and land/topology view.</Typography>
-            <img style={{width: imageWidth, paddingTop: '1vh'}} src={"/switchMapView.png"}/>
+            <img style={{width: imageWidth, paddingTop: '1vh'}} src={"/switchMapView.PNG"}/>
             <Typography variant="body1">You can also toggle the display of buoys. Buoys are displayed as blue map markers which when clicked, display their collected swell data.</Typography>
-            <img style={{width: imageWidth, paddingTop: '1vh'}} src={"/buoyToggle.png"}/>
+            <img style={{width: imageWidth, paddingTop: '1vh'}} src={"/buoyToggle.PNG"}/>
             <br />
-            <img style={{width: imageWidth, paddingTop: '1vh'}} src={"/buoyData.png"}/>
+            <img style={{width: imageWidth, paddingTop: '1vh'}} src={"/buoyData.PNG"}/>
 
 
           </div>
